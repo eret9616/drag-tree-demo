b31abad5ff4f8ad245714c3cc00b3555e211a2a3
huhao7 <947416983@qq.com>
2024-10-31 17:26:50 +0800

    更新CenterTree.vue文件，禁止将元素拖拽为根节点

--- src/components/CenterTree.vue
+++ src/components/CenterTree.vue
@@ -46,10 +46,16 @@ export default {
         return type !== "prev" && type !== "next";
       } else {
         // 禁止将元素拖拽为根节点
-        return dropNode.level !== 1;
+        // return dropNode.level !== 1;
+        return false;
       }
     },
     handleNodeDrop(draggingNode, dropNode, dropType, ev) {
+      debugger;
+      // 如果是不是flex-container，不能添加进去
+      if (dropNode.data.type !== "flex-container") {
+        return;
+      }
       // 拖拽完成后的处理逻辑（通常不需要额外处理）
       this.$emit("node-drop", draggingNode, dropNode, dropType);
     },
